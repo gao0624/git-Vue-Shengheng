554a4ebd41181961e6c1f59f5020cb94ef956523
gao <13839287160@163.com>
2023-10-11 16:51:25 +0800

    提交轮播组件

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <!-- template 里面必须有且仅有一个div -->
   <div id="app">
-    <navbar />
     <Lunbo/>
   </div>
 </template>
 
 <script>
-import Navbar from './views/layout/Navbar.vue';
 import Lunbo from './views/layout/Lunbo.vue';
 
 export default {
-  components: { Navbar , Lunbo},
+  components: {Lunbo},
   data() {
     return {};
   },
--- src/views/layout/Lunbo.vue
+++ src/views/layout/Lunbo.vue
@@ -1,35 +1,71 @@
 <template>
-    <div class="mylunbo">
-        <a href="javascript:;" class="arrow-l">  </a>
-        <a href="javascript:;" class="arrow-r">  </a>
-        <ul style="width: 100%;">
-            <li style="width: 100%;">
-                <img src="https://image1.zhuangku.com/upfile2018/BannerPic/20200821145935_0976.jpg" alt="" />
-            </li>
-            <li style="width: 100%;">
-                <img src="https://image1.zhuangku.com/upfile2018/BannerPic/20191227141904_3360.jpg" alt="" />
-            </li>
-            <li style="width: 100%;">
-                <img src="https://image1.zhuangku.com/upfile2018/BannerPic/20200821150021_4570.jpg" alt="" />
-            </li>
-            <li style="width: 100%;">
-                <img src="	https://image1.zhuangku.com/upfile2018/BannerPic/20191227141904_3360.jpg" alt="" />
-            </li>
-        </ul>
-        <ol class="circle">
-        </ol>
+  <div class="carousel">
+    <div class="slide-wrapper" :style="{ transform: `translateX(-${currentIndex * 100}%)` }">
+      <div class="slide" v-for="(item, index) in items" :key="index">
+        <img :src="item" alt="Slide Image" />
+      </div>
     </div>
+  </div>
 </template>
 
-
 <script>
-    export default {
-        data() {
-            return {};
-        },
+export default {
+  data() {
+    return {
+      items: [
+      "https://image1.zhuangku.com/upfile2018/BannerPic/20200821145935_0976.jpg",
+      "https://image1.zhuangku.com/upfile2018/BannerPic/20191227141904_3360.jpg",
+      "https://image1.zhuangku.com/upfile2018/BannerPic/20200821150021_4570.jpg",
+      "https://image1.zhuangku.com/upfile2018/BannerPic/20191227141904_3360.jpg"
+      ],
+      currentIndex: 0,
+      autoSlideInterval: null,
     };
+  },
+  created() {
+    this.startAutoSlide();
+  },
+  destroyed() {
+    this.stopAutoSlide();
+  },
+  methods: {
+    startAutoSlide() {
+      this.autoSlideInterval = setInterval(() => {
+        this.nextSlide();
+      }, 2000); // 2秒自动轮播
+    },
+    stopAutoSlide() {
+      clearInterval(this.autoSlideInterval);
+    },
+    nextSlide() {
+      this.currentIndex = (this.currentIndex + 1) % this.items.length;
+    },
+  },
+};
 </script>
 
 <style>
+.carousel {
+  overflow: hidden;
+  position: relative;
+  width: 100%;
+}
 
+.slide-wrapper {
+  display: flex;
+  transition: transform 0.5s;
+}
+
+.slide {
+  flex: 0 0 100%;
+}
+
+.slide img {
+  width: 100%;
+  height: auto;
+}
+
+.active {
+  transform: translateX(-100%);
+}
 </style>
