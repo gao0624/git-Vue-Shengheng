01c2cdfeb5f0d0adf0912c491b0e18bed6099552
gao <13839287160@163.com>
2023-10-18 16:26:23 +0800

    fix

--- src/views/contact/Contact.vue
+++ src/views/contact/Contact.vue
@@ -1,19 +1,22 @@
 <template>
     <div>
+        <Pick/>
         <ContactUs/>
         <Map/>
     </div>
 </template>
 
 <script>
-    import ContactUs from '../layout/ContactUs.vue';
+import ContactUs from '../layout/ContactUs.vue';
 import Map from '../layout/Map.vue';
+import Pick from '../layout/pick.vue';
 
     export default{
         name: 'Contact',
         components: {
             Map,
-            ContactUs
+            ContactUs,
+            Pick
         }
     }
 
---
+++ src/views/layout/pick.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="pick">
+        <img src="../../assets/contactUs.jpg">
+    </div>
+
+</template>
+
+
+<script>
+    export default {
+        data(){
+            return {}
+        }
+    }
+
+</script>
+
+
+<style>
+    .pick{
+        margin-top: 50px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    img{
+        max-width: 100%;
+    }
+    
+
+</style>
